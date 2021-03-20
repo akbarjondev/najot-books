33e5259d742e3e7fd619333c4cb31743a2d17e32
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-03-20 16:01:41 +0500

    Add get /books

--- index.js
+++ index.js
@@ -18,20 +18,57 @@ app.get('/', (req, res) => {
 	res.renderFile('index.html')
 })
 
+app.get('/books', async (req, res) => {
+
+	try {
+		
+		const allBooks = await fetch('select * from books')
+
+		res.send({
+			status: 200,
+			data: allBooks
+		})
+
+	} catch(e) {
+		res.send({
+			status: 500,
+			error: e
+		})
+		
+		console.log(e)
+	}
+
+})
+
 app.post('/books', async (req, res) => {
-	
+		
+	const { book_name, book_number, book_author } = req.body
+
 	const INSERT_BOOK = `
 		insert into
 			books (book_name, book_number, book_author)
 		values($1, $2, $3)
+		returning book_id
 		;
 	`
+	try {
 
-	const dataRes = await fetch()
+		const [ dataRes ] = await fetch(INSERT_BOOK, book_name, book_number, book_author)
 
-	res.send({
-		status: 200
-	})
+		res.send({
+			status: 200,
+			data: dataRes
+		})
+
+	} catch(e) {
+
+		res.send({
+			status: 500,
+			error: e
+		})		
+		console.log(e)
+	}
+
 })
 
 app.listen(3003, () => console.log('ready at http://localhost:3003'))
